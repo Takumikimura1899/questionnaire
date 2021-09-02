6395da93fbdd3d8e20effb018ea3799600013354
Takumikimura141899 <kimura141899@gmail.com>
2021-09-03 04:56:32 +0900

    Q4途中まで

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,23 +5,61 @@ import { useForm } from 'react-hook-form';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  const { register } = useForm();
+  const { register, handleSubmit } = useForm();
 
-  const onSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    console.log('submitted!');
+  const onSubmit = (data: FormEvent) => {
+    console.log(data);
   };
 
   return (
     <>
       <h1>プログラミングに関するアンケート</h1>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <label htmlFor="name">Q1. 名前を入力してください。</label>
-          <input id="name" />
-          <input id="name" {...register('name')} />
+          <label htmlFor='name'>Q1. 名前を入力してください。</label>
+          <input id='name' />
+          <input id='name' {...register('name')} />
         </div>
-        <input type="submit" value="アンケートを提出する" />
+
+        <div>
+          <label htmlFor='birth'>Q2. 生年月日を入力してください。</label>
+          <input id='birth' />
+          <input id='birth' {...register('birth')} />
+        </div>
+
+        <div>
+          <span>Q3. 現在プログラミングを学習していますか？</span>
+          <input
+            id='isLearning1'
+            {...register('isLearning')}
+            name='isLearning'
+            type='radio'
+          />
+          <label htmlFor='isLeaning1'>はい</label>
+          <input
+            id='isLearning2'
+            {...register('isLearning')}
+            name='isLearning'
+            type='radio'
+          />
+          <label htmlFor='isLeaning2'>いいえ</label>
+          <input
+            id='isLearning1'
+            {...register('isLearning')}
+            name='isLearning'
+            type='radio'
+          />
+          <label htmlFor='isLeaning1'>はい</label>
+          <input
+            id='isLearning2'
+            {...register('isLearning')}
+            name='isLearning'
+            type='radio'
+          />
+          <label htmlFor='isLeaning2'>いいえ</label>
+        </div>
+
+        <input type='submit' value='アンケートを提出する' />
       </form>
     </>
   );
