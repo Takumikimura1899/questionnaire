953ac2c29ab3b9df6c94d33f76d9e68dcc4bf334
Takumikimura141899 <kimura141899@gmail.com>
2021-09-03 03:55:53 +0900

    react-form

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,17 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log('submitted!');
+  };
+
   return (
     <>
       <h1>プログラミングに関するアンケート</h1>
+      <form onSubmit={onSubmit}>
+        <input type="submit" value="アンケートを提出する" />
+      </form>
     </>
   );
 }
