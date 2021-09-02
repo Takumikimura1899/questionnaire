ab71462291fbf74ce1c8ed868a52771a9cbe628a
Takumikimura141899 <kimura141899@gmail.com>
2021-09-03 04:14:49 +0900

    typescript react-fook-form register

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { FormEvent } from 'react';
+import { useForm } from 'react-hook-form';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  const onSubmit = (e) => {
+  const { register } = useForm();
+
+  const onSubmit = (e: FormEvent) => {
     e.preventDefault();
     console.log('submitted!');
   };
@@ -12,6 +16,11 @@ export default function Home() {
     <>
       <h1>プログラミングに関するアンケート</h1>
       <form onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="name">Q1. 名前を入力してください。</label>
+          <input id="name" />
+          <input id="name" {...register('name')} />
+        </div>
         <input type="submit" value="アンケートを提出する" />
       </form>
     </>
