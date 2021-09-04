5283b7da4451cd59063e85be52bb5f625deb4710
Takumikimura141899 <kimura141899@gmail.com>
2021-09-04 18:21:45 +0900

    正規表現でのバリデーション

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,11 @@ import { useForm } from 'react-hook-form';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const onSubmit = (data: FormEvent) => {
     console.log(data);
@@ -17,46 +21,59 @@ export default function Home() {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor='name'>Q1. 名前を入力してください。</label>
-          <input id='name' />
-          <input id='name' {...register('name')} />
+          <input id='name' {...register('name', { required: true })} />
+          {errors.name && <span>このフィールドは回答必須です。</span>}
         </div>
 
         <div>
           <label htmlFor='birth'>Q2. 生年月日を入力してください。</label>
-          <input id='birth' />
-          <input id='birth' {...register('birth')} />
+          <input
+            id='birth'
+            {...register('birth', { required: true, pattern: /^[0-9]{8}$/ })}
+          />
+          {errors.birth && <span>このフィールドは回答必須です。</span>}
         </div>
 
         <div>
           <span>Q3. 現在プログラミングを学習していますか？</span>
           <input
             id='isLearning1'
-            {...register('isLearning')}
+            {...register('isLearning', { required: true })}
             name='isLearning'
             type='radio'
+            value='true'
           />
           <label htmlFor='isLeaning1'>はい</label>
           <input
             id='isLearning2'
-            {...register('isLearning')}
+            {...register('isLearning', { required: true })}
             name='isLearning'
             type='radio'
+            value='false'
           />
           <label htmlFor='isLeaning2'>いいえ</label>
+          {errors.isLearning && <span>このフィールドは回答必須です。</span>}
+        </div>
+
+        <div>
+          <span>Q4. これまでにプログラミングを学習したことがありますか?</span>
           <input
-            id='isLearning1'
-            {...register('isLearning')}
-            name='isLearning'
+            id='wasLearning1'
+            {...register('wasLearning', { required: true })}
+            name='wasLearning'
             type='radio'
+            value='true'
           />
-          <label htmlFor='isLeaning1'>はい</label>
+          <label htmlFor='wasLeaning1'>はい</label>
           <input
-            id='isLearning2'
-            {...register('isLearning')}
-            name='isLearning'
+            id='wasLearning2'
+            {...register('wasLearning', { required: true })}
+            name='wasLearning'
             type='radio'
+            value='false'
           />
-          <label htmlFor='isLeaning2'>いいえ</label>
+          <label htmlFor='wasLeaning2'>いいえ</label>
+          {errors.wasLearning && <span>このフィールドは回答必須です。</span>}
         </div>
 
         <input type='submit' value='アンケートを提出する' />
